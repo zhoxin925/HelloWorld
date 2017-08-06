38287b379d23c018894c6697bb6bcc56b79a7911
zhoxin925 <2013268994>
2017-08-06 09:15:58 +0800

    tablayout

--- app/src/main/java/com/zhoxin/helloworld/MainActivity.java
+++ app/src/main/java/com/zhoxin/helloworld/MainActivity.java
@@ -3,6 +3,8 @@ package com.zhoxin.helloworld;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.zhoxin.higithub.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
---
+++ app/src/main/java/com/zhoxin/helloworld/coordinatorlayout/TabLayoutView.java
@@ -0,0 +1,19 @@
+package com.zhoxin.helloworld.coordinatorlayout;
+
+import android.os.Bundle;
+
+import com.zhoxin.helloworld.BaseActivity;
+import com.zhoxin.higithub.R;
+
+/**
+ * Created by zhouxin on 2017/8/2.
+ */
+
+public class TabLayoutView extends BaseActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.coordinator_tablayout);
+    }
+}
