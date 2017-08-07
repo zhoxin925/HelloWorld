e96a795d6ed4b7efdad65fcd7fe68aaeb8c47c44
zhoxin925 <2013268994>
2017-08-07 21:38:19 +0800

    tablayout

---
+++ app/src/main/java/com/zhoxin/helloworld/coordinatorlayout/CollapsingToolBarActivity.java
@@ -0,0 +1,10 @@
+package com.zhoxin.helloworld.coordinatorlayout;
+
+import com.zhoxin.helloworld.init.BaseActivity;
+
+/**
+ * Created by zhouxin on 2017/8/7.
+ */
+
+public class CollapsingToolBarActivity extends BaseActivity {
+}
