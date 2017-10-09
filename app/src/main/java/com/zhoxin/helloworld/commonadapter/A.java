fd6265ff1db3ad6e3bba99bf497ea1e35a64cf6e
zhoxin925 <2013268994>
2017-10-09 21:20:00 +0800

    toolbar

---
+++ app/src/main/java/com/zhoxin/helloworld/commonadapter/A.java
@@ -0,0 +1,25 @@
+package com.zhoxin.helloworld.commonadapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by zhouxin on 2017/8/14.
+ */
+
+public class A extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
--- app/src/main/java/com/zhoxin/helloworld/coordinatorlayout/CollapsingToolBarActivity.java
+++ app/src/main/java/com/zhoxin/helloworld/coordinatorlayout/CollapsingToolBarActivity.java
@@ -1,11 +1,18 @@
 package com.zhoxin.helloworld.coordinatorlayout;
 
+import android.os.Bundle;
+
 import com.zhoxin.helloworld.init.BaseActivity;
+import com.zhoxin.higithub.R;
 
 /**
  * Created by zhouxin on 2017/8/7.
  */
 
 public class CollapsingToolBarActivity extends BaseActivity {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.coordinator_collapsingtoolbarlayout);
+    }
 }
